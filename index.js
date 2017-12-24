c91bedcf3f0ed3d8d3f885544fd556017fcd8ba4
Surajit Sarkar <surajitsarkar19@gmail.com>
2017-12-24 10:13:30 +0530

    radial gradient react bridge added

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {processColor,requireNativeComponent, ViewPropTypes,View} from 'react-native';
 
-import { NativeModules } from 'react-native';
+export default class RadialGradient extends Component {
+    static propTypes = {
+        center: PropTypes.arrayOf(PropTypes.number),
+        colors: PropTypes.arrayOf(PropTypes.string),
+        radius: PropTypes.arrayOf(PropTypes.number),
+        ...ViewPropTypes,
+    };
 
-const { RNRadialGradient } = NativeModules;
+    render() {
+        const {
+            children,
+            colors,
+            center,
+            radius,
+            style,
+            ...otherProps
+        } = this.props;
 
-export default RNRadialGradient;
+
+        return (
+            <View ref={(component) => { this.gradientRef = component; }} {...otherProps} style={style}>
+                <NativeRadialGradient
+                    style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0}}
+                    colors={(colors)?colors.map(processColor):null}
+                    center={center}
+                    radius={radius}
+                />
+                { children }
+            </View>
+        );
+    }
+}
+
+
+const NativeRadialGradient = requireNativeComponent('SRSRadialGradient', null);
